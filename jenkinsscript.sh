ccd046423e46b830fb2eae391b420b56d6f83e09
praveendevopsk8s <149125849+praveendevopsk8s@users.noreply.github.com>
2025-04-27 07:47:12 +0530

    Update jenkinsscript.sh

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+. ~/.bashrc
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+ eval "$(pyenv init --path)"
+fi
 
-set -e
+pyenv versions
 
-echo "#### Setting up Python environment ####"
 pyenv global 3.10.12
 python3 -m venv myenv
 source myenv/bin/activate
+echo '#### Checking python ####'
+which python3
+python3 -V
 
-echo "#### Installing requirements ####"
-pip install -r requirements.txt
+echo '#### Install requirements ####'
+pip install -r ./requirements.txt
 pip install pytest-cov
 
-echo "#### Running tests ####"
-mkdir -p xmlReport
+echo '#### Run tests ####'
 pytest --cov=main utests --junitxml=./xmlReport/output.xml
 python -m coverage xml
+
+echo '### deactivate virtual environment ###'
+deactivate
+echo '### change pyenv to system ###'
+pyenv global system
